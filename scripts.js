bb9be6dcd2bc8f1454ace0aa3bd5e0dd07ce4a0f
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-17 09:38:42 +0200

    I have added an event listener to apply search filters

--- scripts.js
+++ scripts.js
@@ -78,6 +78,20 @@ const updateShowMoreButton = () => {
     getElement(selector).open = true;
     if (focusSelector) getElement(focusSelector).focus();
   };
+
+  const applySearchFilters = (filters) => {
+    return books.filter((book) => {
+      const titleMatch =
+        filters.title.trim() === "" ||
+        book.title.toLowerCase().includes(filters.title.toLowerCase());
+      const authorMatch =
+        filters.author === "any" || book.author === filters.author;
+      const genreMatch =
+        filters.genre === "any" || book.genres.includes(filters.genre);
+      return titleMatch && authorMatch && genreMatch;
+    });
+  };
+  
   
     starting.appendChild(element)
 }
